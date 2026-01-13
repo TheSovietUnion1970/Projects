ddb42c36fecb7b1a7b4814d386217f9793500d97
Vinh <vinh@vinh.com>
2026-01-13 20:22:46 +0700

    [BBB][CAN] Update 08_CAN/Remote_frame/

---
+++ 08_CAN/Remote_frame/BBB_beRequested/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/* PROGRAM CONTROL */
+#define WORK_DELAY 1000
+#define DMA_REG 1
+#define DMA_OBJ_MSG_NUM 45
+#define SIZE_REQUESTED 4
+
+/* USER CONTROL */
+#define ID_CAN_TARGET 0x45
+#define DMA_USED 0
+#define PRINT_DEBUG 0
+/* ******* */
+
+#endif
